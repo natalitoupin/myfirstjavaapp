243f23940d3d902ce06a64c8fab9964286bf174f
Natali Toupin <natalitoupin@gmail.com>
2016-11-23 12:23:51 -0500

    Just things I was doing

---
+++ src/NinetyNineBeers.java
@@ -0,0 +1,17 @@
+/**
+ * Created by natalitoupin on 11/18/16.
+ */
+public class NinetyNineBeers {
+    public static void main (String [] args){
+        int beerNum = 99;
+        String word = "bottles";
+
+        while (beerNum > 0){
+
+            if (beerNum == 1){
+                word = "bottles";
+
+            }
+        }
+    }
+}
